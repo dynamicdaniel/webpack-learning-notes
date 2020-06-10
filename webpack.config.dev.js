ad77a73517f715739c050229c8a53dccf0f2cab7
dynamicdaniel <350027912@qq.com>
2020-06-10 13:58:01 +0800

    'testDllPlugin'

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,55 @@
 // 开发环境配置，webpack.config.dev.js，主要是devServer、API mock
 // 相关配置，注重效率，打包速度优化
-
+const fs = require('fs')
+const path = require('path')
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base.js')
-module.exports = webpackMerge({
-}, baseConfig)
+const vendorConfig = require('./webpack.config.vendor.js')
+const webpack = require('webpack')
+const { DllReferencePlugin } = webpack
+module.exports = new Promise((resolve, reject) => {
+  let hasManifest = fs.existsSync('./dist/react.manifest.json')
+  let config = () => webpackMerge({
+    devtool: 'source-map',
+    plugins: [
+      // 告诉 webpack 使用了哪些动态链接库
+      new DllReferencePlugin({
+        // 描述 react 动态链接库的文件内容
+        manifest: require('./dist/react.manifest.json')
+      }),
+    ]
+  }, baseConfig)
+  if (!hasManifest) {
+    webpack(vendorConfig, (err, stats) => {
+      if (err || stats.hasErrors()) {
+        let errInfo = stats.toJson()
+        console.log('创建DLL错误', err, errInfo.errors)
+        return fs.readdir('./dist', (err, files) => {
+          if (err) {
+            console.log(err)
+            return
+          } else {
+            return files.map(v =>
+              new Promise((resolve, reject) => {
+                fs.unlink(path.join(__dirname, 'dist', v), err => {
+                  if(err){
+                    console.error(err)
+                    reject(`删除${v}失败`)
+                  } else {
+                    resolve(`删除${v}成功`)
+                    console.log(`删除${v}成功`)
+                  }
+                })
+              }).catch(err => {
+                reject(err)
+              })
+            )
+          }
+        })
+      }
+      resolve(config())
+    })
+  } else {
+    resolve(config())
+  }
+})
--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
-const { DllPlugin, DllReferencePlugin } = webpack
+const { DllPlugin } = webpack
 
 module.exports = {
   entry: {
     // 将React 相关的模块放到一个单独的动态链接库中
     react: ['react', 'react-dom'],
     // 将项目需要的所有polyfill 放到一个单独的动态链接库中
-    polyfill: ['core-js/fn/object/assign', 'core-js/fn/promise', 'whatwg-fetch'],
+    // polyfill: ['core-js/fn/object/assign', 'core-js/fn/promise', 'whatwg-fetch'],
   },
   output: {
     // 输出的动态链接库的文件名称，[name]代表当前动态链接库的名称，
@@ -17,7 +17,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // 存放动态链接库的全局变量名称，例如对于 react 来说就是 _dll_react
     // 加上_dll，防止全局变量冲突
-    library: '_dll_[name]'
+    library: '_dll_[name]',
     // page 180 - 164
-  }
+  },
+  plugins: [
+    // 接入 DllPlugin
+    new DllPlugin({
+      // 动态链接库的全局变量名称，需要和 output.library 中的保持一致
+      // 该字段的值也就是输出的 manifest.json 文件中 name 字段的值
+      // 例如在 react.manifest.json 中就有 'name': '_dll_react'
+      name: '_dll_[name]',
+      // 描述动态链接库的 manifest.json 文件输出时的名称
+      path: path.join(__dirname, 'dist', '[name].manifest.json')
+    })
+  ]
 }
